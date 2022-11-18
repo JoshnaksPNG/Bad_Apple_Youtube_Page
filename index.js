88f27adcd488233dd6f5cd733088c3927715fafc
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2022-11-18 08:07:58 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+// Libraries
+const jimp = require("jimp");
+const ffmpeg = require("ffmpeg");
+const fs = require("fs");
+const Jimp = require("jimp");
+const lodash = require("lodash");
+
+// Frame Intervals
+const EVERY_N_FRAME_SET = 1;
+const vid_x = 4;
+const vid_y = 3;
+
+const THUMBNAIL_WIDTH = 281;
+const THUMBNAIL_HEIGHT = 158;
+
+const CHANNEL_WIDTH = 36;
+const CHANNEL_HEIGHT = 36;
+
+const HORIZONTAL_MARGIN = 16;
+const VERTICAL_MARGIN = 86;
+const VERTICAL_MINOR = 12;
+
+// Extract Video To JPG frames (Stole from Stack Overflow :D)
+try {
+    let process = new ffmpeg("video_input/input.mp4");
+    process.then( (video) =>
+    {
+        video.fnExtractFrameToJPG("c://git/Bad_Apple_Youtube_Page/frame_output/whole_frames", 
+        {
+            every_n_frames : EVERY_N_FRAME_SET
+        }, resized_frames)
+    }, function (err) 
+    {
+        console.log('Error: ' + err);
+    });
+} 
+catch (e) 
+{
+    console.log(e.code);
+    console.log(e.msg);
+}
+
+async function resized_frames(error, files)
+{
+    // Get Number of Frames
+    let total_frames = files.length;
+
+    // Resize Frames
+    const PIXEL_WIDTH = (HORIZONTAL_MARGIN * (vid_x - 1)) + (THUMBNAIL_WIDTH * vid_x);
+    const PIXEL_HEIGHT = (VERTICAL_MARGIN * (vid_y - 1)) + ((THUMBNAIL_HEIGHT + VERTICAL_MARGIN) * vid_y);
+
+    for(let i = 1; !(i > total_frames); ++i)
+    {
+        const in_path = "c://git/Bad_Apple_Youtube_Page/frame_output/whole_frames/input_" + i + ".jpg";
+        const out_path = "c://git/Bad_Apple_Youtube_Page/frame_output/resized_frames/frame_" + i + ".jpg";
+        const img = await Jimp.read(in_path);
+
+        img.resize(PIXEL_WIDTH, PIXEL_HEIGHT);
+
+        for(let j = 0; j < vid_x; ++j)
+        {
+            for(let k = 0; k < vid_y; ++k)
+            {
+                const sliced_out_path = "c://git/Bad_Apple_Youtube_Page/frame_output/frame_parts/" + i + "";
+                const thumb_path = sliced_out_path + "/" + k + "," + j + "thumb.jpg";
+                const channel_path = sliced_out_path + "/" + k + "," + j +"channel.jpg";
+
+                
+                const timg = lodash.cloneDeep(img);
+                const cimg = lodash.cloneDeep(img);
+                timg.crop
+                (
+                    (j * (THUMBNAIL_WIDTH + HORIZONTAL_MARGIN)),
+                    (k * (THUMBNAIL_HEIGHT + VERTICAL_MARGIN)),
+                    THUMBNAIL_WIDTH,
+                    THUMBNAIL_HEIGHT
+                ).write(thumb_path);
+
+                cimg.crop
+                (
+                    (j * (THUMBNAIL_WIDTH + HORIZONTAL_MARGIN)),
+                    ((THUMBNAIL_HEIGHT + VERTICAL_MINOR) + ((THUMBNAIL_HEIGHT + VERTICAL_MARGIN) * k)),
+                    CHANNEL_WIDTH,
+                    CHANNEL_HEIGHT
+                ).write(channel_path);
+            }
+        }
+    }
+
+
+}
---
+++ play.js
@@ -0,0 +1,43 @@
+const VID_X = 4;
+const VID_Y = 3;
+
+const VID_DIM = VID_X * VID_Y;
+
+const FRAMERATE = 35;
+const FRAME_INTERVAL = ((1 / FRAMERATE) * 1000);
+
+const FRAME_COUNT = 6955;
+
+let frame = 0;
+
+const imgs = document.querySelectorAll(".yt-img-shadow");
+
+//update_frame();
+
+setInterval(play_video, FRAME_INTERVAL);
+
+function update_frame()
+{
+    for(let i = 0; i < VID_X; ++i)
+    {
+        for(let j = 0; j < VID_Y; ++j)
+        {
+            const channel_path = "file://C:/git/Bad_Apple_Youtube_Page/frame_output/frame_parts/" + (frame + 1) + "/" + j + "," + i + "channel.jpg";
+            const thumbnail_path = "file://C:/git/Bad_Apple_Youtube_Page/frame_output/frame_parts/" + (frame + 1) + "/" + j + "," + i + "thumb.jpg";
+        
+            const index = (j * VID_X) + i + 0; //console.log(thumbnail_path);
+            imgs[(index * 2) + 1].src = thumbnail_path; 
+            imgs[(index * 2) + 2].src = channel_path;
+        }
+    }
+}
+
+function play_video()
+{
+    update_frame();
+    ++frame;
+    if(frame == FRAME_COUNT)
+    {
+        frame = 0;
+    }
+}
